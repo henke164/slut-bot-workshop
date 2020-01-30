f62d5f7ebcbc5792040e5d20a430f3771afcdb5f
henke164 <henrik.mattsson@hotmail.com>
2020-01-30 08:49:24 +0100

    socket login

--- modules/ModuleLab/services/loginService.js
+++ modules/ModuleLab/services/loginService.js
@@ -1,31 +1,98 @@
-// THIS IS EXAMPLE CODE
-const fetch = require("node-fetch");
-const {
-  getCookieStringFromResponse
-} = require("../../../utilities/cookieHelper");
-const { userAgentHeader, jsonHeader } = require("../../../utilities/headers");
+const puppeteer = require("puppeteer");
+const WebSocket = require("ws");
+const uuid = require("uuid/v1");
+
+async function getSessionId() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto(`https://www.dunder.com/`);
+
+  return new Promise(resolve => {
+    let intervalMs = 2000;
+    const interval = setInterval(async () => {
+      const cookies = await page.cookies();
+      const sidCookie = cookies.find(c => c.name === "rn_sid-dunder");
+      if (!sidCookie) {
+        console.log("Waiting for cookie...");
+        return;
+      }
+
+      console.log("Sid found!");
+      await browser.close();
+      clearInterval(interval);
+      resolve(sidCookie.value);
+    }, intervalMs);
+  });
+}
+
+async function getLoginPayload(payloadId, user) {
+  const sid = await getSessionId();
+  const dunderToken = "59ad361053746c7c87715b8c";
+  return {
+    id: payloadId,
+    sid: sid,
+    path: "/igc/dunder/users/loginV2",
+    params: {
+      usernameOrEmail: user.email,
+      password: user.password
+    },
+    options: {
+      isDevice: false,
+      baseUrl: "wss://api.dunder.com",
+      token: dunderToken,
+      db: "dunder",
+      provider: "igc",
+      proxies: ["maintenance"]
+    },
+    token: dunderToken,
+    navigator: {
+      appCodeName: "Mozilla",
+      appName: "Netscape",
+      appVersion:
+        "5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+      language: "en-US",
+      platform: "Win32",
+      product: "Gecko",
+      productSub: "20030107",
+      userAgent:
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36",
+      vendor: "Google Inc.",
+      vendorSub: ""
+    }
+  };
+}
 
 async function login(user) {
-  const body = {
-    username: user.username, // or user.email
-    password: user.password
-  };
+  return new Promise(resolve => {
+    const ws = new WebSocket("wss://api.dunder.com/");
 
-  const response = await fetch("casino-login-url", {
-    method: "POST",
-    headers: {
-      ...userAgentHeader,
-      ...jsonHeader,
-      origin: "casino-url"
-    },
-    body: JSON.stringify(body)
+    console.log("Connecting to socket...");
+    const payloadId = uuid().replace("-", "");
+
+    ws.on("open", async () => {
+      console.log("Connected");
+      console.log("Creating login payload...");
+      const loginPayload = await getLoginPayload(payloadId, user);
+      ws.send(JSON.stringify(loginPayload));
+      console.log("Loginpayload sent!");
+    });
+
+    ws.on("message", async data => {
+      const responsePayload = JSON.parse(data.toString("utf8"));
+      if (responsePayload.id !== payloadId) {
+        return;
+      }
+
+      if (!responsePayload.res.sessionId) {
+        console.log("Error occured!", responsePayload);
+        resolve(null);
+        return;
+      }
+
+      resolve(`rn_sid-dunder=${responsePayload.res.sessionId};`);
+    });
   });
-
-  if (response.status !== 200) {
-    throw Error("Login failed!");
-  }
-
-  return getCookieStringFromResponse(response);
 }
 
 module.exports = {
