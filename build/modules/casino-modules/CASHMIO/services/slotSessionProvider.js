1f276dff922a7ec9247a5c06adadb4ed7818e420
Henrik Matsson <henrik.mattsson@hotmail.com>
2020-01-31 10:28:11 +0100

    cashmio fix

--- build/modules/casino-modules/CASHMIO/services/slotSessionProvider.js
+++ build/modules/casino-modules/CASHMIO/services/slotSessionProvider.js
@@ -1,21 +1,26 @@
-// THIS IS EXAMPLE CODE
 const fetch = require("node-fetch");
 const { userAgentHeader, jsonHeader } = require("../../../utilities/headers");
 
 async function getSlotSession(slotId, casinoCookies) {
-  const response = await fetch(`https://www.cashmio.com/account/get_sessionId`, {
-    method: "GET",
-    headers: {
-      ...userAgentHeader,
-      ...jsonHeader,
-      "referer": "https://www.cashmio.com/en/casino/game/${slotId}",
-      "cookie": casinoCookies
+  const response = await fetch(
+    `https://games-cashmio.igamingcloud.com/es/${slotId}`,
+    {
+      method: "GET",
+      headers: {
+        ...userAgentHeader,
+        ...jsonHeader,
+        referer: "https://www.cashmio.com/en/casino/game",
+        cookie: casinoCookies
+      }
     }
-  });
+  );
 
-  const sid = await response.json();
+  const html = await response.text();
+  const foundSession = /"sessionId": "(?:[^"\\]|\\.)*/
+    .exec(html)[0]
+    .replace('"sessionId": "', "");
 
-  return sid.sid;
+  return foundSession;
 }
 
 module.exports = {
